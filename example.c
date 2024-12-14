524920c91a320998ecf1bd4b8bac5ef8eecde0f3
Eduardo Costa <m4c0@users.noreply.github.com>
2024-12-14 16:38:27 +0000

    "env" returning non-const
    Helps inform this is a dup

--- example.c
+++ example.c
@@ -4,6 +4,12 @@
 #include "sysstd.h"
 
 int main() {
+  char * env = sysstd_env("HOME");
+  if (env) {
+    printf("HOME = %s\n", env);
+    free(env);
+  }
+
   if (0 != sysstd_mkdir("out/test")) return 1;
 
   FILE * f = sysstd_fopen("out/test/file", "w");
