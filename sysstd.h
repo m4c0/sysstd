2948fdebe1fbb6ecdefd5e344e97b931a5d5a020
Eduardo Costa <m4c0@users.noreply.github.com>
2024-12-14 16:34:55 +0000

    getenv/dupenv

--- sysstd.h
+++ sysstd.h
@@ -9,26 +9,37 @@
 #define SYSSTD_FILE_SEP "/"
 #endif
 
-FILE * sysstd_fopen(const char * name, const char * mode);
-int    sysstd_mkdir(const char * path);
+const char * sysstd_env(const char * name);
+FILE *       sysstd_fopen(const char * name, const char * mode);
+int          sysstd_mkdir(const char * path);
 
 #ifdef SYSSTD_IMPLEMENTATION
 
 #ifdef _WIN32
 #include <direct.h>
 
+const char * sysstd_env(const char * name) {
+  char *buf;
+  size_t sz;
+  return (0 == _dupenv_s(&buf, &sz, name)) ? buf : NULL;
+}
 FILE * sysstd_fopen(const char * name, const char * mode) {
   FILE * res;
-  if (0 != fopen_s(&res, name, mode)) return NULL;
-  return res;
+  return (0 == fopen_s(&res, name, mode)) ? res : NULL;
 }
 int sysstd_mkdir(const char * path) {
   return _mkdir(path);
 }
 
 #else // !_WIN32
+#include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 
+const char * sysstd_env(const char * name) {
+  const char * e = getenv(name);
+  return e ? strdup(e) : NULL;
+}
 FILE * sysstd_fopen(const char * name, const char * mode) { return fopen(name, mode); }
 int sysstd_mkdir(const char * path) { return mkdir(path, 0777); }
 
